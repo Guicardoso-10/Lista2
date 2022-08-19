ef573b436e9e16cc1042c8903085c9753d540103
Guicardoso-10 <guiguicardoso-10@hotmail.com>
2022-08-18 21:05:17 -0300

    7/11

---
+++ exerc7.js
@@ -0,0 +1,8 @@
+console.log("programa que lê e mostra 5 números digitados")
+import {questionInt} from 'readline-sync'
+const numero1 = questionInt("Digite o primeiro numero: ")
+const numero2 = questionInt("Digite o segundo numero: ")
+const numero3 = questionInt("Digite o terceiro numero: ")
+const numero4 = questionInt("Digite o quarto numero: ")
+const numero5 = questionInt("Digite o quinto numero: ")
+console.log("os números são: " + numero1 + " " + numero2 + " " + numero3 + " " + numero4 + " " + numero5)
