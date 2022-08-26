224c041b2314610b5c82f594097b774f62bf3091
Gutemberg <gutemberg.jhonata@gmail.com>
2022-08-25 21:01:20 -0300

    Correção exercício 10

--- exerc10.js
+++ exerc10.js
@@ -1,17 +1,23 @@
 import {questionInt} from 'readline-sync'
+
 let  lista = []
+
 for (let i = 1; i <= 5; i++) {
     const numero = questionInt("Digite um numero inteiro: ")
     lista.push(numero)
 }
+
 console.log(lista)
-let soma = 0
-for (let i = 0; i <= 4; i++) {
-    soma = soma + lista[i]
+
+let soma = lista[0]
+let multiplicacao = lista[0]
+
+for (let i = 1; i < lista.length; i++) {
+    // soma = soma + lista[i]
+    soma += lista[i]
+    // multiplicacao = multiplicacao * lista[i]
+    multiplicacao *= lista[i]
 }
+
 console.log("a soma dos números é: " + soma)
-let multiplicação = lista[0]
-for (let i = 1; i <= 4; i++) {
-    multiplicação = multiplicação * lista[i]
-}
-console.log("a multiplicação dos números é: " + multiplicação)
+console.log("a multiplicação dos números é: " + multiplicacao)
